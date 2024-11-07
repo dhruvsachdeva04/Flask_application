aea4642020a6945da4cf120e4963f6df19bde07c
dhruvsachdeva04 <dhruvsachdeva3828@gmail.com>
2024-11-07 16:34:19 +0530

    run.py

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+# Add the parent directory to sys.path
+sys.path.append(str(Path(__file__).resolve().parent))
+
 from app import create_app
 
 app = create_app()
